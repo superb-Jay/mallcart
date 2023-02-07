09f0ad1df054b4a133b5a88934aade47dce309b6
Jay <myride.jay@gmail.com>
2023-02-07 20:10:22 +0900

    initial commit

---
+++ src/main/java/com/example/mallcart/MallcartApplication.java
@@ -0,0 +1,13 @@
+package com.example.mallcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallcartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallcartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mallcart/MallcartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mallcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallcartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
