86db3f8adec993935ca35882d80751ad9ac16dd6
Jay <myride.jay@gmail.com>
2023-02-07 20:30:23 +0900

    feat: entity init

--- src/main/java/com/example/mallcart/entity/Cart.java
+++ src/main/java/com/example/mallcart/entity/Cart.java
@@ -1,2 +1,31 @@
-package com.example.mallcart.entity;public class Cart {
+package com.example.mallcart.entity;
+
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.*;
+
+@Builder
+@Entity
+@Table(name ="prac_cart")
+@Getter
+@RequiredArgsConstructor
+
+public class Cart {
+
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="amount")
+    private int amount;
+
+    @ManyToOne(targetEntity = Product.class, fetch = FetchType.EAGER)
+    @JoinColumn(name = "pro_id")
+    private Product product;
+
+
 }
--- src/main/java/com/example/mallcart/entity/Product.java
+++ src/main/java/com/example/mallcart/entity/Product.java
@@ -1,2 +1,38 @@
-package com.example.mallcart.entity;public class Product {
+package com.example.mallcart.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="prac_product")
+@Builder
+@Getter
+@RequiredArgsConstructor
+public class Product {
+
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="intro")
+    private String intro;
+
+    @Column(name="contents")
+    private String contents;
+
+    @Column(name="image")
+    private String image;
+
+    @Column(name="size")
+    private String size;
+
+    @Column(name="color")
+    private String color;
+
+    @Column(name="price")
+    private int price;
 }
